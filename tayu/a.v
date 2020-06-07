12520fb02579fb072cd67219738ecc5730f39952
mio2 <motty.mio2@gmail.com>
2020-06-07 20:34:53 +0900

    tayu

---
+++ tayu/a.v
@@ -0,0 +1,19 @@
+module test(A,Y,VALID);
+    input[3:0] A;
+    output [1:0] Y;
+    output VALID;
+
+    function [1:0] encode;
+        input [3:0] a;
+
+        /*
+        encode[1] = a[3];
+        encode[0] = (~a[2] & a[1]) | a[3];
+        */
+
+        encode = {a[3],(~a[2] & a[1]) | a[3]};
+    endfunction
+
+    assign Y = encode(A);
+    assign VALID = |A;
+endmodule
